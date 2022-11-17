0e641ab9b2bc661741efddf4134325ab946b6f8c
Thiniraxz <thiniragenuka@gmail.com>
2022-11-18 02:22:25 +0530

    Create functions to reset inidcators

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,30 +1,68 @@
-export const removeDuplicates = (arr) => {
-  const uniqueData = []
+import {
+  updateCryptoChartData,
+  updateCryptoDataLimit,
+  updateCryptoTimeStamp,
+  updateIndicatorData,
+  updateExternalIndicatorData,
+} from "../redux/chart";
 
-  const unique = arr.filter(element=>{
+export const removeDuplicates = (arr) => {
+  const uniqueData = [];
+
+  const unique = arr.filter((element) => {
     var time = element.time.toString().split(".")[0];
     const isDuplicate = uniqueData.includes(time);
 
-    if(!isDuplicate){
+    if (!isDuplicate) {
       uniqueData.push(time);
       return true;
     }
-    return false
-  })
+    return false;
+  });
   return unique;
-
-}
+};
 
 export const compare = (a, b) => {
   // Use toUpperCase() to ignore character casing
-  const timeA = a.time
-  const timeB = b.time
+  const timeA = a.time;
+  const timeB = b.time;
 
-  let comparison = 0
+  let comparison = 0;
   if (timeA > timeB) {
-    comparison = 1
+    comparison = 1;
   } else if (timeA < timeB) {
-    comparison = -1
+    comparison = -1;
   }
-  return comparison
-}
+  return comparison;
+};
+
+export const resetInternalIndicators = (dispatch) => {
+  dispatch(updateIndicatorData({ indicatorType: "ma", data: [] }));
+  dispatch(updateIndicatorData({ indicatorType: "sma", data: [] }));
+  dispatch(updateIndicatorData({ indicatorType: "wma", data: [] }));
+  dispatch(updateIndicatorData({ indicatorType: "ema", data: [] }));
+  dispatch(
+    updateIndicatorData({
+      indicatorType: "bbands",
+      data: { upper: [], middle: [], lower: [] },
+    })
+  );
+};
+
+export const resetExternalIndicatorData = (dispatch) => {
+  dispatch(updateExternalIndicatorData({ indicatorType: "rsi", data: [] }));
+  dispatch(updateExternalIndicatorData({ indicatorType: "obv", data: [] }));
+  dispatch(updateExternalIndicatorData({ indicatorType: "roc", data: [] }));
+  dispatch(
+    updateExternalIndicatorData({
+      indicatorType: "macd",
+      data: { series: [], signalSeries: [], histSeries: [] },
+    })
+  );
+  dispatch(
+    updateExternalIndicatorData({
+      indicatorType: "stoch",
+      data: { slowk: [], slowd: [] },
+    })
+  );
+};
