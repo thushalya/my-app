4b6ecbddd5d88b3a8787195a0d93e0d0596c5e53
AwesomeNipun <nipun1969@gmail.com>
2022-11-19 20:54:34 +0530

    remove google login

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import { onMessageListener } from "./firebaseInit";
 import ReactNotificationComponent from "./views/notification/ReactNotifications";
 import { useDispatch } from "react-redux";
 import { increment } from "./redux/notification";
-// import TokenRequest from "../src/views/notification/TokenRequest";
-// import { onMessageListener } from "./utils/firebaseInit";
-// import {useEffect, useState} from 'react';
-// import {onMessageListener} from "./firebaseInit"
 
-const CLIENT_ID = "1061743313688-iokgsqk1gm07khha74tq9evt4k798ucf.apps.googleusercontent.com";
 
 function App() {
   const [show, setShow] = useState(false);
@@ -20,19 +15,12 @@ function App() {
 
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   if (show) {
-  //     dispatch(increment());
-  //   }
-  // }, [show]);
   onMessageListener()
     .then((payload) => {
       console.log("hey hey hey a new notification has arvied", payload);
       callDispatch();
       setShow(true);
-      // toast.success(`${payload.notification.body}`)
       setNotification({
-        // title: 'New notification',
         title: payload.notification.title,
         body: payload.notification.body,
       });
@@ -43,15 +31,6 @@ function App() {
   const callDispatch = () => {
     dispatch(increment());
   };
-  useEffect(() => {
-    function start() {
-      gapi.auth2.init({
-        client_id: CLIENT_ID,
-        scope: "",
-      });
-    }
-    gapi.load("client:auth2", start);
-  });
 
   return (
     <div className="App">
